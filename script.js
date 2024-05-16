5e626f05798f1b2c0785381014d585a70f1ae147
DineshGunasekaran <95580000+DineshGunasekaran@users.noreply.github.com>
2024-05-16 21:33:14 +0530

    Add files via upload
    Random password generator - Implemeted with dynamic method of selection charactors

---
+++ script.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById('password');
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const specialChar = '!@#$%^&*(){}|"';
+const allChar = upperCase + lowerCase + number + specialChar;
+let password;
+function generatePassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += specialChar[Math.floor(Math.random() * specialChar.length)];
+
+  while (length > password.length) {
+    password += allChar[Math.floor(Math.random() * allChar.length)];
+  }
+
+  passwordBox.value = password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  console.dir(passwordBox.value);
+  let copytext = passwordBox.value;
+  navigator.clipboard.writeText(copytext);
+}
